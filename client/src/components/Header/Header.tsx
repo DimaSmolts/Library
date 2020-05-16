b514f32c54b98259cee56208a28a46f66076c26f
Dmytro_Smolts <dima.smolts@gmail.com>
2020-05-17 00:53:12 +0300

    default margings/paddings added, logos in Header added

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Header.css'
 
+const libraryLogo = require('../../assets/images/library-logo.svg');
+const shoppingBag = require('../../assets/images/shopping-bag.svg');
+
 export default class Header extends React.Component {
   render() {
     return <div className='header'>
-      <span>Library</span>
+      <img src={libraryLogo} />
+      <p>Library</p>
+      <img src={shoppingBag} />
     </div>
   }
 }
