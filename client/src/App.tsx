eefeb173d8bac916c673f0e226457eeed0ece7be
Dmytro_Smolts <dima.smolts@gmail.com>
2020-05-16 18:58:02 +0300

    Header & Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import './App.css'
+import './common-styles/index.css'
 import BookListPage from './components/BookListPage/BookListPage'
 import BookPage from './components/BookPage/BookPage'
 import PurchasePage from './components/PurchasePage/PurchasePage'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 export default class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path={['/', '/books']} component={BookListPage} />
-          <Route exact path='/books/:bookId' render={(routeProps) => <BookPage bookId={routeProps.match.params.bookId} />} />
-          <Route exact path='/purchase' component={PurchasePage} />
-          <Route path='/*' render={() => <h1>not found</h1>} />
-        </Switch>
-      </BrowserRouter>
+      <div className='layout'>
+        <Header />
+        <div className='content'>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path={['/', '/books']} component={BookListPage} />
+              <Route exact path='/books/:bookId' render={(routeProps) => <BookPage bookId={routeProps.match.params.bookId} />} />
+              <Route exact path='/purchase' component={PurchasePage} />
+              <Route path='/*' render={() => <h1>not found</h1>} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+        <Footer />
+      </div>
     )
   }
 }
---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+
+export default class Footer extends React.Component {
+  render() {
+    return <div className='footer'>
+      <p className='year'>2020</p>
+      <p className='author'>by Dmytro Smolts</p>
+    </div>
+  }
+}
