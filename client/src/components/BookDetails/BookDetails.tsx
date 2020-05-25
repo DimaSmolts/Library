389ed06ad5575404d04511a5d92710dda8b147ef
Dmytro_Smolts <dima.smolts@gmail.com>
2020-05-26 00:52:36 +0300

    BookDetails markup & styles improvment

--- client/src/components/BookDetails/BookDetails.tsx
+++ client/src/components/BookDetails/BookDetails.tsx
@@ -2,18 +2,33 @@ import React from 'react';
 import { Book } from '../../helpers/BookModel'
 import './index.css'
 
+const starSolid = require('../../assets/images/star-solid.svg');
+const starOutline = require('../../assets/images/star-outline.svg');
+
 interface BookDetailsProps {
-  book: Book
+  book: Book,
 }
 
-export default class BookDetails extends React.Component<BookDetailsProps, {}> {
+export default class BookDetails extends React.Component<BookDetailsProps> {
   render() {
     return <div className='book-detail-container'>
       <div className='book-detail-image'>
         <img src={this.props.book.image} />
       </div>
       <div className='book-detail-rate'>
-        rate:{this.props.book.rate}
+        {
+          (() => {
+            let stars = Array<JSX.Element>();
+            for (let i = 0; i < 10; i++) {
+              if (i < this.props.book.rate) {
+                stars.push(<img src={starSolid} />);
+              } else {
+                stars.push(<img src={starOutline} />);
+              }
+            }
+            return stars;
+          })()
+        }
       </div>
       <h3 className='book-detail-author'>
         {this.props.book.author}
@@ -21,11 +36,20 @@ export default class BookDetails extends React.Component<BookDetailsProps, {}> {
       <h2 className='book-detail-name'>
         {this.props.book.name}
       </h2>
-      <div className='book-detail-price'>
-        {this.props.book.price}
+      <div className='book-detail-genres'>
+        {
+          this.props.book
+            .genres.map(genre => {
+              return <span className='book-detail-genre'>{genre}</span>
+            })
+        }
       </div>
       <div className='book-detail-description'>
         {this.props.book.description}
+      </div>
+      <div className='book-detail-purchase'>
+        <span className='book-detail-price'>{this.props.book.price}£</span>
+        <button className='book-detail-purchase-button'>Add to cart</button>
       </div>
     </div>
   }
