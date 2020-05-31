e9e267fe0ec0bed797db3a65ba1dd48537206d8f
Dmytro_Smolts <dima.smolts@gmail.com>
2020-06-01 00:59:14 +0300

    client dev-server moved to .env

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,10 +1,13 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+require('dotenv').config();
+
+const port = process.env.REACT_APP_CLIENT_PORT || 9000;
 
 module.exports = merge(common, {
     mode: 'development',
     devServer: {
-        port: 9000,
+        port: port,
         historyApiFallback: true
     },
     devtool: 'inline-source-map',
