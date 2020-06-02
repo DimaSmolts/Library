5355208465eafb89a5c2d947411d147dd3d2ca06
Dmytro_Smolts <dima.smolts@gmail.com>
2020-06-02 22:43:35 +0300

    tests configurations

---
+++ client/jest.config.js
@@ -0,0 +1,25 @@
+const { defaults } = require('jest-config');
+
+module.exports = {
+  roots: ["<rootDir>/src"],
+  modulePathIgnorePatterns: ["<rootDir>/src/tests"],
+  collectCoverage : true,
+  coverageReporters: ["text", "lcov"],
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}",
+    "!**/coverage/**",
+    "!**/dist/**",
+    "!**/node_modules/**",
+    "!<rootDir>/src/test-tools/**"
+  ],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: [...defaults.moduleFileExtensions,"ts", "tsx"],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  setupTestFrameworkScriptFile: "<rootDir>/setupEnzyme.ts",
+  moduleNameMapper: {
+    "\\.css": "<rootDir>/src/test-tools/stylesMock.ts",
+  }
+}
